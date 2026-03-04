c6cc88fb722859c0bb6a91eab738046d3e5d4fe2
nurpolat06 <nurkozybagar@gmail.com>
2026-03-04 11:09:13 +0500

    Изменение цены

--- lib/main.dart
+++ lib/main.dart
@@ -440,9 +440,9 @@ class ProfileScreen extends StatelessWidget {
 
 final List<Map<String, dynamic>> phoneProducts = [
   {'name': 'iPhone 15 Pro', 'price': 649990, 'image': 'https://ir.ozone.ru/s3/multimedia-1-o/7129349196.jpg'},
-  {'name': 'Samsung Galaxy S24 Ultra', 'price': 599990, 'image': 'https://ir.ozone.ru/s3/multimedia-f/w1200/6896605947.jpg'},
+  {'name': 'Samsung Galaxy S24 Ultra', 'price': 450000, 'image': 'https://ir.ozone.ru/s3/multimedia-f/w1200/6896605947.jpg'},
   {'name': 'Xiaomi 14 Ultra', 'price': 499990, 'image': 'https://avatars.mds.yandex.net/get-mpic/15106342/2a00000196b4e64d596c1649841cc2ccd54b/orig'},
   {'name': 'HUAWEI Pura 70 Ultra', 'price': 549990, 'image': 'https://i.ebayimg.com/images/g/3T0AAOSw3otoS-O9/s-l500.jpg'},
   {'name': 'Nothing Phone (2)', 'price': 320000, 'image': 'https://avatars.mds.yandex.net/get-mpic/1526692/2a0000018e743c24e02eaeff0eaff8ef9cf1/orig'},
-  {'name': 'Google Pixel 8 Pro', 'price': 420000, 'image': 'https://avatars.mds.yandex.net/get-mpic/8382397/2a0000019059a4c75a09fa81eb207bb1a146/orig'},
+  {'name': 'Google Pixel 8 Pro', 'price': 420000, 'image': 'https://avatars.mds.yandex.net/get-mpic/8382397/2a0000019059a4c75a09fa81eb207bb1a146/orig'}, 
 ];
